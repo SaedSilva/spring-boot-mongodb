702a68e183c1fc2f843ed50e817c725e7f149bf8
Saed Sousa <saed.sousa@hotmail.com>
2024-05-17 10:21:03 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/saeddev/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.saeddev.workshopmongo.repository;
+
+import com.saeddev.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
