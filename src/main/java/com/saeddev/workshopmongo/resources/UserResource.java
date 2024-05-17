aea542589fa1c81c4f73ba607dac23176d91c65c
Saed Sousa <saed.sousa@hotmail.com>
2024-05-16 21:15:11 -0300

    Obtendo um usuário por id

--- src/main/java/com/saeddev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/saeddev/workshopmongo/resources/UserResource.java
@@ -5,11 +5,11 @@ import com.saeddev.workshopmongo.dto.UserDTO;
 import com.saeddev.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,5 +25,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> dtoList = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(dtoList);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
