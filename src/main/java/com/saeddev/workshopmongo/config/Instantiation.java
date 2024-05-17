1fad2a05cf22394c448cd35029c4a2fc4c027699
Saed Sousa <saed.sousa@hotmail.com>
2024-05-17 10:49:12 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/saeddev/workshopmongo/config/Instantiation.java
+++ src/main/java/com/saeddev/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.saeddev.workshopmongo.config;
 import com.saeddev.workshopmongo.domain.Post;
 import com.saeddev.workshopmongo.domain.User;
 import com.saeddev.workshopmongo.dto.AuthorDTO;
+import com.saeddev.workshopmongo.dto.CommentDTO;
 import com.saeddev.workshopmongo.repository.PostRepository;
 import com.saeddev.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,10 +37,19 @@ public class Instantiation implements CommandLineRunner {
         postRepository.deleteAll();
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
         userRepository.save(maria);
+
+
 
     }
 }
--- src/main/java/com/saeddev/workshopmongo/resources/PostResource.java
+++ src/main/java/com/saeddev/workshopmongo/resources/PostResource.java
@@ -4,11 +4,10 @@ import com.saeddev.workshopmongo.domain.Post;
 import com.saeddev.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/posts")
